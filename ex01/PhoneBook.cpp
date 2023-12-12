0e2caf4a91ac37152960c33dacce98ad719b8c55
Ambroise Bareux <abareux@k2r1p9.42mulhouse.fr>
2023-12-12 14:52:19 +0100

    redule line

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -46,19 +46,11 @@ std::string PhoneBook::truncated(const std::string str) {
 void PhoneBook::ft_search(void)
 {
   std::cout << "#----------#----------#----------#----------#" << std::endl;
-
-  std::cout << "|" << std::right << std::setw(10) << "index" 
-            << "|" << std::setw(10) << "first name" 
-            << "|" << std::setw(10) << "last name"
-            << "|" << std::setw(10) << "nickname" << "|" << std::endl;
-
+  std::cout << "|" << std::right << std::setw(10) << "index" << "|" << std::setw(10) << "first name" << "|" << std::setw(10) << "last name" << "|" << std::setw(10) << "nickname" << "|" << std::endl;
   std::cout << "#----------#----------#----------#----------#" << std::endl;
 
   for (int i = 0; i < 8; i++) {
-    std::cout << "|" << std::right << std::setw(10) << i
-              << "|" << std::setw(10) << truncated(ListContact[i].getFirstName())
-              << "|" << std::setw(10) << truncated(ListContact[i].getLastName())
-              << "|" << std::setw(10) << truncated(ListContact[i].getNickname()) << "|" << std::endl;
+    std::cout << "|" << std::right << std::setw(10) << i << "|" << std::setw(10) << truncated(ListContact[i].getFirstName()) << "|" << std::setw(10) << truncated(ListContact[i].getLastName()) << "|" << std::setw(10) << truncated(ListContact[i].getNickname()) << "|" << std::endl;
     std::cout << "#----------#----------#----------#----------#" << std::endl;
   }
   int index = 0;
